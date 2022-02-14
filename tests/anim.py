f16b24d69b01770dedd816524c793ea48ad00676
yansnow78 <ygrugni@gmail.com>
2022-02-14 14:17:58 +0100

    add/clean anim test

--- tests/anim.py
+++ tests/anim.py
@@ -6,7 +6,7 @@ alien.images = ['alien_walk1', 'alien_walk2']
 alien.fps = 3
 
 alien2 = Actor('alien_walk1')
-alien2.load_images('alien2', 2, 1, subrect=(0, 0, 134, 96))
+alien2.load_images('alien_walk_spritesheet', 2, 1, subrect=(0, 0, 134, 96))
 alien2.fps = 5
 alien2.topleft = 100, 0
 
