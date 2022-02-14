62d2075e43841292bc742746fde93da231413104
yansnow78 <ygrugni@gmail.com>
2022-02-14 14:02:34 +0100

    Revert "correct line_obb_dist_squared and line_circle in class Collide"
    This reverts commit a1e66f005daf2a77a44f54480d8ad91ae1604623.

--- tests/circle_line.py
+++ tests/circle_line.py
@@ -6,50 +6,24 @@ import time
 WIDTH=800
 HEIGHT=600
 
-mouse_pos = [0, 0]
-mouse_pressing = False
-
-def on_mouse_move(pos):
-    global mouse_pos
-    if mouse_pressing:
-        mouse_pos = pos
-    
-def on_mouse_down(pos):
-    global mouse_pressing
-    mouse_pressing = True
-
-def on_mouse_up(pos):
-    global mouse_pressing
-    mouse_pressing = False
-    
 def update():
     pass
 
-# l1x1 = 65  # random.randint(100, 700)
-# l1y1 = 80  # random.randint(100, 500)
-# l1x2 = l1x1  # random.randint(100, 700)
-# l1y2 = 250  # andom.randint(100, 500)
-l1x1 = random.randint(100, 400)
-l1y1 = random.randint(100, 300)
-l1x2 = l1x1+random.randint(100, 400)
-l1y2 = l1y1#+random.randint(100, 400)
-r    = 60 # random.randint(30, 150)
-# c = [
-#     120,  # random.randint(100, 700),
-#     180, # mouse_pos[1],  # random.randint(100, 500),
-#     r
-# ]
-        
 def draw():
-    # time.sleep(0.5)
+    time.sleep(0.5)
 
     screen.clear()
 
     c = [
-        mouse_pos[0],
-        mouse_pos[1],
-        r
+        random.randint(100, 700),
+        random.randint(100, 500),
+        random.randint(10, 150)
     ]
+
+    l1x1 = random.randint(100, 700)
+    l1y1 = random.randint(100, 500)
+    l1x2 = random.randint(100, 700)
+    l1y2 = random.randint(100, 500)
 
     if Collide.circle_line(c[0], c[1], c[2], l1x1, l1y1, l1x2, l1y2):
         color = (255, 0, 0)
